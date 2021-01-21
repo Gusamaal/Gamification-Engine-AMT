c8b4bc27cf10bbbf0331d4373a926a5dca19f2f4
alban_favre <fav.alban2@gmail.com>
2021-01-21 18:10:59 +0100

    first badge test

--- gamification-specs/src/test/java/ch/heigvd/gamification/api/spec/steps/RuleSteps.java
+++ gamification-specs/src/test/java/ch/heigvd/gamification/api/spec/steps/RuleSteps.java
@@ -1,7 +1,11 @@
 package ch.heigvd.gamification.api.spec.steps;
 
+import ch.heigvd.gamification.ApiException;
 import ch.heigvd.gamification.api.DefaultApi;
+import ch.heigvd.gamification.api.dto.Rule;
 import ch.heigvd.gamification.api.spec.helpers.Environment;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 
 public class RuleSteps {
 
@@ -10,6 +14,8 @@ public class RuleSteps {
     private BasicSteps basicSteps;
     private ApplicationSteps applicationSteps;
     private UserSteps userSteps;
+
+    private Rule rule;
 
 
     // I might need to call the steps in a cascad way: call applicationSteps from Usersteps
@@ -20,5 +26,29 @@ public class RuleSteps {
         this.basicSteps = basicSteps;
         this.applicationSteps = applicationSteps;
         this.userSteps = userSteps;
+    }
+
+    @Given("I have a badge rule payload")
+    public void i_have_a_rule_payload() {
+        rule = new ch.heigvd.gamification.api.dto.Rule()
+                ._if(new ch.heigvd.gamification.api.dto.RuleIf()
+                .eventType("sickAction"))
+        .then(new ch.heigvd.gamification.api.dto.RuleThen()
+                .action(new ch.heigvd.gamification.api.dto.RuleThenAction()
+                        .name("addBadge")
+                        .target("red")
+                        .amount(0)
+                )
+        ); //this is pure insanity
+    }
+
+    // we need to get the created user id
+    @When("^I POST the rule payload to the /rules endpoint$")
+    public void i_POST_the_rule_payload_to_the_rules_endpoint() {
+        try {
+            basicSteps.processApiResponse(api.createRuleWithHttpInfo(applicationSteps.getApiKey(), rule));
+        } catch (ApiException e) {
+            basicSteps.processApiException(e);
+        }
     }
 }
--- gamification-specs/src/test/resources/scenarios/rule.feature
+++ gamification-specs/src/test/resources/scenarios/rule.feature
@@ -2,3 +2,11 @@ Feature: User operations on rules
 
   Background:
     Given there is a Gamification server
+
+    Scenario: create a badge rule
+      Given I have an application payload
+      When I POST the application payload to the /applications endpoint
+      Given I have a correct API key
+      Given I have a badge rule payload
+      When I POST the rule payload to the /rules endpoint
+      Then I receive a 201 status code
